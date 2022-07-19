3d52cbd6b847b9a6d1089a013ae6b60345b02f13
Aliaksandra Bandarenka <aliaksandra.bandarenka@pandadoc.com>
2022-07-19 14:56:12 +0300

    add auth context to propagate login info

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import React, {useEffect, useReducer, useState} from 'react';
+import React, {useContext, useEffect, useReducer, useState} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from "../../store/auth-context";
+import Input from "../UI/Input/Input";
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -24,11 +26,13 @@ const passwordReducer = (state, action) => {
   return { value: '', isValid: false };
 };
 
-const Login = (props) => {
+const Login = () => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer,{ value: '', isValid: null });
   const [passwordState, dispatchPassword] = useReducer(passwordReducer,{ value: '', isValid: null });
+
+  const authCtx = useContext(AuthContext);
 
   const { isValid: isEmailValid } = emailState;
   const { isValid: isPasswordValid } = passwordState;
@@ -55,42 +59,32 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-              isEmailValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
+        <Input
             id="email"
+            type="email"
+            name="E-Mail"
             value={emailState.value}
+            isValid={isEmailValid}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            isPasswordValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
+        />
+        <Input
             id="password"
+            type="password"
+            label="Password"
             value={passwordState.value}
+            isValid={isPasswordValid}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
-          />
-        </div>
+        />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" disabled={!formIsValid}>
             Login
           </Button>
         </div>
---
+++ src/store/auth-context.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from "react";
+
+const AuthContext = React.createContext({
+    isLoggedIn: false,
+    onLogin: (email, password) => {},
+    onLogout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const isUserLoggedIn = localStorage.getItem('isLoggedIn');
+        if (isUserLoggedIn === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const loginHandler = (email, password) => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        localStorage.setItem('isLoggedIn', '0');
+        setIsLoggedIn(false);
+    };
+
+    return <AuthContext.Provider
+        value={{
+            isLoggedIn,
+            onLogin: loginHandler,
+            onLogout: logoutHandler,
+        }}
+    >{props.children}
+    </AuthContext.Provider>
+}
+
+export default AuthContext;
